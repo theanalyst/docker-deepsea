902523f2ae2db6b9428ae235b1b897cea5f3176f
Abhishek Lekshmanan <abhishek@suse.com>
2017-02-06 14:08:41 +0100

    run: use the correct param subs syntax
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DOCKER_NETWORK=${DOCKER_NETWORK:-ceph}
-BUILD=${1:$RANDOM}
-MINION_COUNT=${MINION_COUNT:3}
-OSD_SIZE=${OSD_SIZE:1000}
+BUILD=${1:-$RANDOM}
+MINION_COUNT=${MINION_COUNT:-3}
+OSD_SIZE=${OSD_SIZE:-1000}
+echo "this is build:$BUILD"
 docker network list | grep $DOCKER_NETWORK || docker network create $DOCKER_NETWORK --subnet 10.0.0.0/16 --gateway 10.0.0.1
 docker run -d --tmpfs --tmpfs /run -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --name $BUILD-deepsea-salt-master --hostname $BUILD-deepsea-salt-master deepsea/salt-master
 for i in {1..$MINION_COUNT}; do
