bb048fe491987ee1c66fec606e765fab90252df7
Abhishek Lekshmanan <abhishek@suse.com>
2017-02-06 13:23:54 +0100

    add loop devices
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 DOCKER_NETWORK=${DOCKER_NETWORK:-ceph}
 BUILD=${1:$RANDOM}
 MINION_COUNT=${MINION_COUNT:3}
+OSD_SIZE=${OSD_SIZE:1000}
 docker network list | grep $DOCKER_NETWORK || docker network create $DOCKER_NETWORK --subnet 10.0.0.0/16 --gateway 10.0.0.1
 docker run -d --tmpfs --tmpfs /run -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --name $BUILD-deepsea-salt-master --hostname $BUILD-deepsea-salt-master deepsea/salt-master
-
 for i in {1..$MINION_COUNT}; do
-    docker run -d --tmpfs --tmpfs /run -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --name $BUILD-deepsea-salt-minion-$i --hostname $BUILD-deepsea-salt-minion-$i deepsea/salt-minion
+    dd if=/dev/zero of=test-$build-$i.img bs=1M count=$OSD_SIZE
+    sudo losetup /dev/disk-$build-$i $build-$i.img
+    docker run -d --tmpfs --tmpfs /run -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --device /dev/disk-$build-$i --name $BUILD-deepsea-salt-minion-$i --hostname $BUILD-deepsea-salt-minion-$i deepsea/salt-minion
 done
