170b8efbc6f1dadfa18cb274271894644a8cc404
Abhishek Lekshmanan <abhishek@suse.com>
2017-02-06 14:09:10 +0100

    use the correct docker network
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- run.sh
+++ run.sh
@@ -5,9 +5,11 @@ MINION_COUNT=${MINION_COUNT:-3}
 OSD_SIZE=${OSD_SIZE:-1000}
 echo "this is build:$BUILD"
 docker network list | grep $DOCKER_NETWORK || docker network create $DOCKER_NETWORK --subnet 10.0.0.0/16 --gateway 10.0.0.1
-docker run -d --tmpfs --tmpfs /run -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --name $BUILD-deepsea-salt-master --hostname $BUILD-deepsea-salt-master deepsea/salt-master
-for i in {1..$MINION_COUNT}; do
-    dd if=/dev/zero of=test-$build-$i.img bs=1M count=$OSD_SIZE
-    sudo losetup /dev/disk-$build-$i $build-$i.img
-    docker run -d --tmpfs --tmpfs /run -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --device /dev/disk-$build-$i --name $BUILD-deepsea-salt-minion-$i --hostname $BUILD-deepsea-salt-minion-$i deepsea/salt-minion
+docker run -d --tmpfs /tmp --tmpfs /run -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --name $BUILD-deepsea-salt-master --hostname $BUILD-deepsea-salt-master --network $DOCKER_NETWORK --ip 10.0.0.10 deepsea/salt-master
+sudo losetup -D
+for i in {1..3}; do
+    dd if=/dev/zero of=test-$BUILD-$i.img bs=1M count=$OSD_SIZE
+    sleep 10
+    sudo losetup /dev/loop$i test-$BUILD-$i.img
+    docker run -d --tmpfs /tmp --tmpfs /run -v /sys/fs/cgroup/:/sys/fs/cgroup:ro --device /dev/loop$i --name $BUILD-deepsea-salt-minion-$i --hostname $BUILD-deepsea-salt-minion-$i --network $DOCKER_NETWORK deepsea/salt-minion
 done
